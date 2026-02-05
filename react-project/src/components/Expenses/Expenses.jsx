a153e59045183c3e4b66b5226f29bcb287cabaa4
Laura <laura-liis.parm@voco.ee>
2026-02-05 16:48:45 +0200

    ül 12ä

--- react-project/src/components/Expenses/Expenses.jsx
+++ react-project/src/components/Expenses/Expenses.jsx
@@ -1,16 +1,29 @@
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem.jsx";
+import "./ExpenseItem.jsx";
 import Card from "../../../UI/Card.jsx";
+import ExpensesFilter from "./ExpensesFilter.jsx";
 
-function Expenses(props) {
-  return (
-    <Card className="expenses">
-      {}
-      {props.expenses.map((expense) => (
-        <ExpenseItem key={expense.id} expenseData={expense} />
-      ))}
-    </Card>
-  );
+function ExpenseList(props) {
+  return null;
 }
 
+const Expenses = (props) => {
+  const [filteredExpenses, setFilteredExpenses] = useState(props.expenses);
+
+  const filterChangeHandler = (year) => {
+    console.log(`year is ${year}`);
+
+    const newArray = props.expenses.filter(
+        (expense) => new Date(expense.date).getFullYear() === year,
+    );
+    setFilteredExpenses(newArray);
+  };
+
+  return (
+      <Card className="expenses">
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
+        <ExpenseList expenses={filteredExpenses}></ExpenseList>
+      </Card>
+  );
+};
 export default Expenses;
